9a86aca2795680436c8e924dc20c2fd4bb17db53
zigg17 <jacob.ziegler@colorado.edu>
2025-03-25 21:38:41 -0600

    update: creation of UI, axed folder logic -> need to reestablish

--- data-processing/main.py
+++ data-processing/main.py
@@ -1,17 +1,27 @@
+import curses
 import utils.pytorch as torch_util
 import utils.video_proc as video_util
-import utils.deeplabcut as dlc_util
 import utils.terminal as terminal_util
 
-bbox_pth = "models/bbox_cage.pth"
-model, transform, device = torch_util.initialize_model(bbox_pth)
+def main(stdscr):
+    terminal_util.opening_page(stdscr)
 
-while True:
-    folder_path, file_list = video_util.get_directory()
-    if folder_path is not None:
-        break
+    bbox_pth = "models/bbox_cage.pth"
+    model, transform, device = torch_util.initialize_model(bbox_pth)
 
-desktop_folders = video_util.process_videos_to_frames(file_list)
-desktop_folders = video_util.bbox_folders(model, transform, device, desktop_folders)
+    terminal_util.step1_func(stdscr)
 
-video_util.vid_processor(desktop_folders)
+    desktop_folders = video_util.process_videos_to_frames(file_list)
+    desktop_folders = video_util.bbox_folders(model, transform, device, desktop_folders)
+
+    video_util.vid_processor(desktop_folders)
+
+    import utils.deeplabcut as dlc_util
+
+    # You can even do a closing curses screen if you want
+    stdscr.clear()
+    stdscr.addstr(0, 0, "Processing complete. Press any key to exit.")
+    stdscr.refresh()
+    stdscr.getch()
+
+curses.wrapper(main)
